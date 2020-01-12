988e48d48fe29f8f1ebb41d0f11b7bb5870eabe1
onewayticket255 <15365182027@163.com>
2020-01-12 22:52:44 +0800

    init

---
+++ main.js
@@ -0,0 +1,35 @@
+const axios = require('axios')
+const moment = require('moment')
+const { GistBox } = require('gist-box')
+const { gist_id, gist_token } = process.env
+const box = new GistBox({ id: gist_id, token: gist_token })
+const time = moment().utcOffset("+08:00").format('YYYY-MM-DD kk:mm')
+
+let content = ''
+
+const bloglist = [
+    {
+        name: '某科学的最后之作',
+        url: 'http://yunfwe.cn/',
+        regex: /post-title-link.*?href="(.*?)"/
+    },
+]
+
+axios(bloglist[0]['url']).then(res => {
+    const regex = bloglist[0]['regex']
+    const tmp = regex.exec(res.data)
+    const latestPost = tmp[1]
+    console.log(latestPost)
+    if (latestPost) {
+        content = bloglist[0]['name']+'\n'+latestPost + '\n\n' + time
+    }
+    else {
+        content = 'Blog DOM Changed' + '\n\n' + time
+    }
+
+}).then(() => {
+    box.update({ content })
+})
+
+
+
